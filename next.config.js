7ee7abf02e0bd2ec4b979d3cfa6475d7366c4616
Joseph Maldjian <josephavm@icloud.com>
2024-04-03 09:39:02 -0500

    chore: :fire: clear codebase for project re-write

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  typescript: {
-    ignoreBuildErrors: true
-  }
-}
-
-module.exports = nextConfig
--- src/app/instance/[node]/[instance]/files/loading.js
+++
@@ -1,9 +0,0 @@
-import { Flex, Loader } from "@mantine/core-app";
-
-export default async function Loading() {
-    return (
-        <Flex mt="xs">
-            <Loader mx="auto" />
-        </Flex>
-    )
-}
